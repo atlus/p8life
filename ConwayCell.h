e202d4b67c901b27032624d17217e1f5a296d87c
MikePena <mikepena@gmail.com>
2009-11-27 17:47:12 -0600

    R9 Conway Test Cases Added and Documented

--- ConwayCell.h
+++ ConwayCell.h
@@ -1,10 +1,16 @@
 class ConwayCell : public AbstractCell
 {
 	public:
+	/**
+	*Default Constructor
+	*/
 		ConwayCell()
 		{
 		}
-
+/**
+*Constructor takes in char to determine if it's alive or not
+*@param x Character passed in
+*/
 		ConwayCell(char x)
 		{
 			if (x == '.')
@@ -12,14 +18,20 @@ class ConwayCell : public AbstractCell
 			else
 				alive = true;
 		}
-
+/**
+* Returns cell symbol
+*@return Returns the Fredkin symbol
+*/
 		char symbol()
 		{
 			if (alive)
 				return '*';
 			return '.';
 		}
-
+/**
+*Attempts to evolve
+*@return Returns flag if evolved or not
+*/ 
 		int evolve()
 		{
 			if (!alive && neighbors == 3)
